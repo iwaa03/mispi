4d753ef6ae5019f83c8b6d9dcbaffc49ea172452
Nikita Ivanov <iwaa0303@mail.ru>
2023-04-17 16:39:08 +0300

    r19

--- src/A.java
+++ src/A.java
@@ -27,4 +27,8 @@ public class A implements C {
     public java.util.Random mm() {
         return new java.util.Random();
     }
+
+    public byte oo() {
+        return 3;
+    }
 }
--- src/F.java
+++ src/F.java
@@ -27,4 +27,8 @@ public class F implements C {
     public double ee() {
         return Math.PI;
     }
+
+    public int cc() {
+        return 13;
+    }
 }
